19867018d46a4ce7a6969415d319e818959a0c6d
Quentin Golsteyn <qgolsteyn@gmail.com>
2018-05-22 23:03:39 -0700

    Added centering in Storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,13 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import centered from '@storybook/addon-centered';
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /.stories.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+// Centers the stories in the frame
+addDecorator(centered);
 
 configure(loadStories, module);
