ff7bf760c187b054dfe3a23a1681ac1d494a585c
Quentin Golsteyn <qgolsteyn@gmail.com>
2018-05-22 23:41:19 -0700

    Added themes for Button

--- stories/Button.stories.js
+++ stories/Button.stories.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
 
 import Button from '../lib/elements/Button';
 
 storiesOf('Button', module)
-    .add('with text', () => <Button>Hello Button</Button>);
+    .add('default', () => <Button>Hello Button</Button>)
+    .add('primary', () => <Button appearance="primary">Hello Button</Button>);
